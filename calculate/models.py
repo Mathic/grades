19e4bd1a7d50df6ae32c26cb82854a5b1dea957b
Riza Baltazar <rizbalt@gmail.com>
2017-10-16 23:29:12 -0400

    :hamster:  Progress :cat:
    - created Mark and Course models
    - delete section function
    - add, delete, view course html pages and functionality
    - refactoring

--- calculate/models.py
+++ calculate/models.py
@@ -7,17 +7,44 @@ from django.utils.translation import ugettext_lazy as _
 # user: spysauce
 # password: password123
 # Create your models here.
+class Course(models.Model):
+    course_code = models.CharField(max_length=8, default='PHYS1001')
+    course_text = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.course_text
+
+
 class Section(models.Model):
-    section_text = models.CharField(max_length=200)
+    section_text = models.CharField(max_length=50)
     percentage = models.IntegerField(default=0,
                                      validators=[
                                          MaxValueValidator(100),
                                          MinValueValidator(1)
                                      ])
     models.ForeignKey('self')
+    models.ForeignKey('Course', related_name='course', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.section_text
+
+
+class Assignment(models.Model):
+    assignment_name = models.CharField(max_length=50)
+    mark = models.IntegerField(default=0)
+    total = models.IntegerField(default=0)
+    percentage = models.IntegerField(default=0,
+                                     validators=[
+                                         MaxValueValidator(100),
+                                         MinValueValidator(1)
+                                     ])
+    models.ForeignKey('Section', on_delete=models.CASCADE)
+
+
+class CourseForm(ModelForm):
+    class Meta:
+        model = Course
+        fields = ['course_code', 'course_text']
 
 
 class SectionForm(ModelForm):
@@ -30,3 +57,9 @@ class SectionForm(ModelForm):
         help_texts = {
             'percentage': _('Your mark percentage for this section in the course.'),
         }
+
+
+class AssignmentForm(ModelForm):
+    class Meta:
+        model = Assignment
+        fields = ['assignment_name', 'mark', 'total', 'percentage']
--- calculate/views.py
+++ calculate/views.py
@@ -1,32 +1,70 @@
-from django.shortcuts import render
 from django.http import Http404, HttpResponseRedirect
+from django.shortcuts import render
+from django.views.generic import DeleteView
+from django.urls import reverse
 
-from .models import Section, SectionForm
+from .models import Section, SectionForm, Course, CourseForm
 
 
 # Create your views here.
 def index(request):
+    course_list = Course.objects.all()[:25]
     section_list = Section.objects.all()[:25]
     context = {
         'section_list': section_list,
+        'course_list': course_list,
     }
     return render(request, 'calculate/index.html', context)
 
 
-def detail(request, section_id):
+def course_details(request, course_id):
+    try:
+        course = Course.objects.get(pk=course_id)
+    except Course.DoesNotExist:
+        raise Http404("Course does not exist")
+    return render(request, 'calculate/course_details.html', {'course': course})
+
+
+def add_course(request):
+    if request.method == 'POST':
+        course_form = CourseForm(request.POST)
+        if course_form.is_valid():
+            course_form.save()
+            return HttpResponseRedirect('/calculate')
+    else:
+        course_form = SectionForm()
+    return render(request, 'calculate/add_course.html', {'course_form': course_form})
+
+
+class CourseDelete(DeleteView):
+    model = Course
+
+    def get_success_url(self):
+        return reverse('calculate:index')
+
+
+def section_details(request, section_id):
     try:
         section = Section.objects.get(pk=section_id)
     except Section.DoesNotExist:
         raise Http404("Section does not exist")
-    return render(request, 'calculate/detail.html', {'section': section})
+    return render(request, 'calculate/section_details.html', {'section': section})
 
 
-def add(request):
+def add_section(request):
     if request.method == 'POST':
-        form = SectionForm(request.POST)
-        if form.is_valid():
-            form.save()
+        section_form = SectionForm(request.POST)
+        if section_form.is_valid():
+
+            section_form.save()
             return HttpResponseRedirect('/calculate')
     else:
-        form = SectionForm()
-    return render(request, 'calculate/add.html', {'form': form})
+        section_form = SectionForm()
+    return render(request, 'calculate/add_section.html', {'section_form': section_form})
+
+
+class SectionDelete(DeleteView):
+    model = Section
+
+    def get_success_url(self):
+        return reverse('calculate:index')
